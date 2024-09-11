b0d8feb1f97b9e9eebfe37ca5d40d911fbe1bbaf
Ákos Klobetz <klobetz@gmail.com>
2024-09-11 18:02:12 +0200

    # blazor app készítése

---
+++ BlazorApp_To_Api/ViewModel/WeatherForecastVM.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_To_Api.ViewModel
+{
+    public class WeatherForecastVM
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
